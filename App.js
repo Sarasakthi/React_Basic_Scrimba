ede3cfd9dbefa49bb478475c7f070fc91ebffb16
Sarasakthi <saraswathi1404@gmail.com>
2023-11-02 01:32:05 -0600

    02Nov 1:30 am

---
+++ App.js
@@ -0,0 +1,16 @@
+export default function myFunctions() {
+    return (
+        <>
+            const navbar = (
+            <nav>
+                <h1>Bob's Bistro</h1>
+                <ul>
+                    <li>Menu</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            )
+        </>
+    )
+}
